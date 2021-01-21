76fc562c2471994daea5658ddd86496a5b6a29bb
taivy <aie803ef4g@gmail.com>
2021-01-22 02:08:59 +0300

    calc results for first 3 answers

--- src/controllers/testResults.js
+++ src/controllers/testResults.js
@@ -5,6 +5,77 @@ import response from '../helpers/response';
 function calcScores(answers) {
 	let planning, execution, communication, learning, agency, awareness, estimations = 0, 0, 0, 0, 0, 0, 0;
 	
+	if (answers[0].choice.id == "Zkcyzjho0xDc") {
+		planning += 3;
+		execution += 1;
+		learning += 1;
+		awareness += 6;
+	} else if (answers[0].choice.id == "ZWaaVbkqnO5q") {
+		planning += 3;
+		execution += 1;
+		learning += 1;
+		awareness += 6;
+	}  else if (answers[0].choice.id == "t04AoDXfj1UC") {
+		planning += 1;
+		execution += 1;
+		awareness += 6;
+	}  else if (answers[0].choice.id == "LLu6oeYgHgsA") {
+		planning += 1;
+		execution += 1;
+		awareness += 6;
+	}  else if (answers[0].choice.id == "DWKyOsVeEoZE") {
+		planning -= 3;
+		execution += 1;
+	}
+
+	if (answers[1].choice.id == "DdZVCnyQCMf0") {
+		planning_2 = 3;
+		execution += 10;
+		agency += 2;
+	} else if (answers[1].choice.id == "RUJgZvn7HHAr") {
+		planning_2 = 2;
+		execution += 8;
+		agency += 2;
+		estimations += 2;
+	}  else if (answers[1].choice.id == "T8mtIq1QnRV4") {
+		planning_2 = -1;
+		execution += 3;
+		learning += 1;
+		agency += 2;
+	}  else if (answers[1].choice.id == "2an7uBsMdQoE") {
+		planning_2 = -3;
+		execution -= 1;
+	}
+	planning += planning_2;
+
+
+	if (answers[2].choice.id == "3lI8N1okry8Q") {
+		planning += planning_2*0.3;
+		awareness += 2;
+	} else if (answers[2].choice.id == "4ZGyjSM7iLw0") {
+		planning += planning_2*0.8;
+		awareness += 2;
+	}  else if (answers[2].choice.id == "7GX4HGrXFXHR") {
+		planning += planning_2*1.2;
+		awareness += 2;
+	}  else if (answers[2].choice.id == "qbz0Nu58WYzf") {
+		planning += planning_2*2;
+	}
+
+	if (answers[2].choice.id == "3lI8N1okry8Q") {
+		planning += planning_2*0.3;
+		awareness += 2;
+	} else if (answers[2].choice.id == "4ZGyjSM7iLw0") {
+		planning += planning_2*0.8;
+		awareness += 2;
+	}  else if (answers[2].choice.id == "7GX4HGrXFXHR") {
+		planning += planning_2*1.2;
+		awareness += 2;
+	}  else if (answers[2].choice.id == "qbz0Nu58WYzf") {
+		planning += planning_2*2;
+	}
+
+
 
 }
 
