77de10c5efb1d133ead617ce2893b5f4a07b1fd2
taivy <aie803ef4g@gmail.com>
2021-04-09 00:35:39 +0300

    make work with tilda

--- simple_server/public/script.js
+++ simple_server/public/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function() {
-  const el = document.getElementById("ratio-competence-test");
+  //const el = document.getElementById("ratio-competence-test");
   const formUrl = "https://kpbwd338my8.typeform.com/to/Edtmnimf";
   
   const popup = window.typeformEmbed.makePopup(formUrl, {
@@ -10,7 +10,8 @@ window.addEventListener("DOMContentLoaded", function() {
     onSubmit: function (event) {
       console.log('Typeform successfully submitted')
       const response_id = event.response_id;
-      const url = `http://64.227.43.113:9000/get-test-results`;
+      //const url = `http://64.227.43.113:9000/get-test-results`;
+      const url = `https://pay.zaresh.ai/api/get-test-results`;
 
       const config = {
         method: 'POST',
@@ -24,11 +25,26 @@ window.addEventListener("DOMContentLoaded", function() {
           console.log("resp", resp)
           resp.json().then((data) => {
             console.log("data", data);
-            el.innerHTML = "";
+            //el.innerHTML = "";
             let resultsChart = data.resultsChart;
             var img = new Image();
-        img.src = resultsChart;
-        el.appendChild(img);
+            img.src = resultsChart;
+            img.style.margin='30px';
+            //el.appendChild(img);
+
+            let popup = document.querySelectorAll('[data-qa="popup-mode-popup"]');
+            if (popup.length > 0) {
+              popupElem = popup[0];
+              let iframe = document.querySelector( '[data-qa="popup-mode-popup"] iframe' );
+              iframe.parentNode.removeChild(iframe);
+              var d=document.createElement('div');
+              d.style.width='100%';
+              d.style.height='100%';
+              d.style.backgroundColor='white';
+
+              d.appendChild(img);
+              popupElem.appendChild(d);
+            }
           })
         }).catch((err) => {
           console.log("err", err)
@@ -37,9 +53,20 @@ window.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+  /*
   document.getElementById('bt-popup').addEventListener('click', function () {
     popup.open();
   });
+  */
+  console.log("!11!!1")
+  let el = document.querySelectorAll('[href*="mock_url"]');
 
+  if (el.length > 0) {
+    el[0].addEventListener('click', function(e){
+      e.preventDefault();
+      popup.open();
+    });
+  }
 
 });
+  console.log("???")
