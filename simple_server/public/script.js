dd22c72f18a6b5cbb0f199110bec6b441273792e
taivy <aie803ef4g@gmail.com>
2021-04-12 00:53:28 +0300

    fix image position (center), add image text

--- simple_server/public/script.js
+++ simple_server/public/script.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
   //const el = document.getElementById("ratio-competence-test");
   const formUrl = "https://kpbwd338my8.typeform.com/to/Edtmnimf";
+  const imgText = "Вот твои компетенции, сохрани эту картинку";
   
   const popup = window.typeformEmbed.makePopup(formUrl, {
     hideFooter: false,
@@ -29,7 +30,7 @@ window.addEventListener("DOMContentLoaded", function() {
             let resultsChart = data.resultsChart;
             var img = new Image();
             img.src = resultsChart;
-            img.style.margin='30px';
+            img.style.margin='10px';
             //el.appendChild(img);
 
             let popup = document.querySelectorAll('[data-qa="popup-mode-popup"]');
@@ -37,11 +38,20 @@ window.addEventListener("DOMContentLoaded", function() {
               popupElem = popup[0];
               let iframe = document.querySelector( '[data-qa="popup-mode-popup"] iframe' );
               iframe.parentNode.removeChild(iframe);
+
+              var imgTextElem=document.createElement('p');
+              imgTextElem.style.margin='0';
+              imgTextElem.innerText=imgText;
+
               var d=document.createElement('div');
               d.style.width='100%';
               d.style.height='100%';
               d.style.backgroundColor='white';
-
+              d.style.display='flex';
+              d.style.alignItems='center';
+              d.style.justifyContent='center';
+              d.style.flexDirection='column';
+              d.appendChild(imgTextElem);
               d.appendChild(img);
               popupElem.appendChild(d);
             }
